bd46d882f6cc35f62a090fdfb6dfa73d49fe67fd
st-user <nck.cd.339@gmail.com>
2020-11-21 22:51:54 +0900

    babelの導入

--- webpack.common.js
+++ webpack.common.js
@@ -16,6 +16,16 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
   plugins: [
