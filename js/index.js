230338657c8b9d60c2be4e83cc04c7ed937ee423
st-user <nck.cd.339@gmail.com>
2020-12-11 09:58:36 +0900

    BEM,sassの導入

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 require('../node_modules/nouislider/distribute/nouislider.css');
 require('../css/reset.css');
 require('../css/main.css');
+require('../scss-dest/header.css');
+require('../scss-dest/explanations.css');
+require('../scss-dest/color-control-area.css');
 
 import main from './main.js';
 main();
